fa99c98912a89f813cf64cbde1819ef87d47ac38
Alejandro Cruz Valera <alejandro.cruzv@elektra.com.mx>
2023-06-15 17:24:21 -0600

    Configuracion de Main.go y los handlers

--- main.go
+++ main.go
@@ -2,22 +2,77 @@ package main
 
 import (
 	"context"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 
-	/* "os"
-	"strings"
 	"backend/lamda-golang-backend-gambit/awsgo"
-	"backend/lamda-golang-backend-gambit/bd"
-	"backend/lamda-golang-backend-gambit/handlers" */
+	"backend/lamda-golang-backend-gambit/db"
+	"backend/lamda-golang-backend-gambit/handlers"
+	"os"
 
 	lambda "github.com/aws/aws-lambda-go/lambda"
 )
 
+/*
+Ejecucion de la lambda
+*/
 func main() {
 	lambda.Start(ExecuteLambda)
 }
 
+/*
+Logica de la lambda
+*/
 func ExecuteLambda(ctx context.Context, request events.APIGatewayV2HTTPRequest) (*events.APIGatewayProxyResponse, error) {
+	// Se inicializa AWS
+	awsgo.InitializeAWS()
 
+	// Se validan las variables de entorno
+	if !ValidateParams() {
+		panic("Error en los parametros; debe enviar 'SecretName', 'UrlPrefix'")
+	}
+
+	// Se obtienen los valores para los Handlers
+	var res *events.APIGatewayProxyResponse
+	path := strings.Replace(request.RawPath, os.Getenv("UrlPrefix"), "", -1)
+	method := request.RequestContext.HTTP.Method
+	body := request.Body
+	headers := request.Headers
+
+	// Se obtiene el valor del secreto de SecretManager
+	db.ReadSecret()
+
+	status, message := handlers.Handlers(path, method, body, headers, request)
+
+	headersResp := map[string]string{
+		"content-Type": "application/json",
+	}
+
+	res = &events.APIGatewayProxyResponse{
+		StatusCode: status,
+		Body:       string(message),
+		Headers:    headersResp,
+	}
+
+	return res, nil
+}
+
+/*
+Validar si las variables de entorno de la Lambda son devueltas por AWS
+*/
+func ValidateParams() bool {
+	_, getParam := os.LookupEnv("SecretName")
+
+	if !getParam {
+		return getParam
+	}
+
+	_, getParam = os.LookupEnv("UrlPrefix")
+
+	if !getParam {
+		return getParam
+	}
+
+	return getParam
 }
